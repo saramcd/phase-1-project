2ff4866035cc8cc3f91c41351e761dd57a1684e1
Sara McDonnell <saramcdonnell32@gmail.com>
2023-03-10 11:48:03 -0500

    add some styling and an alert

--- script.js
+++ script.js
@@ -12,48 +12,57 @@ document.addEventListener("DOMContentLoaded", () => {
     breweryElement.innerHTML = "";
     const breweryInfoElement = document.getElementById("brewery-info");
     breweryInfoElement.innerHTML = "";
-    const clickForInfoElement = document.createElement("p");
-    clickForInfoElement.innerText = "Click a brewery for more information.";
-    breweryForm.appendChild(clickForInfoElement);
     // Fetch brewery data by state
     fetch(
       `https://api.openbrewerydb.org/breweries?by_state=${state}&per_page=200`
     )
       .then((resp) => resp.json())
       .then((data) => {
-        // Populate HTML with requested state brewery data
-        const stateHeader = document.createElement("h3");
-        stateHeader.innerText = state;
-        breweryElement.append(stateHeader);
-        data.forEach((brewery) => {
-          const li = document.createElement("li");
-          li.innerText = brewery.name;
-          breweryElement.appendChild(li);
-          li.addEventListener("click", (event) => {
-            const breweryClicked = event.target.innerText;
-            const breweryData = data.filter(function (brewery) {
-              return brewery.name == breweryClicked;
-            });
-            const h2 = document.createElement("h2");
-            breweryInfoElement.append(h2);
-            h2.innerText = breweryClicked;
-            const breweryDataObj = breweryData[0];
-            const attributesList = ["brewery_type", "city", "street"];
-            for (const attr in breweryDataObj) {
-              const breweryInfoLi = document.createElement("li");
-              if (attributesList.includes(attr)) {
-                breweryInfoLi.innerText = `${attr}: ${breweryDataObj[attr]}`;
-                breweryInfoElement.appendChild(breweryInfoLi);
+        if (!data.length) {
+          // Alert the user if no breweries found for the state entered
+          alert(
+            `No breweries found for ${state}! Please enter one of the 50 states within the USA.`
+          );
+        } else {
+          // Populate HTML with requested state brewery data;
+          const stateHeader = document.createElement("h3");
+          stateHeader.innerText = `Breweries in ${state}:`;
+
+          breweryElement.append(stateHeader);
+          // Create a List Item for each brewery name
+          data.forEach((brewery) => {
+            const li = document.createElement("li");
+            li.innerText = brewery.name;
+            li.setAttribute("id", brewery.id);
+            breweryElement.appendChild(li);
+            // Add a click event listener to each brewery
+            li.addEventListener("click", (event) => {
+              const breweryClicked = event.target.innerText;
+              const breweryData = data.filter(function (brewery) {
+                return brewery.name == breweryClicked;
+              });
+              const h2 = document.createElement("h2");
+              breweryInfoElement.append(h2);
+              h2.innerText = breweryClicked;
+              const breweryDataObj = breweryData[0];
+              const attributesList = ["brewery_type", "city", "street"];
+              for (const attr in breweryDataObj) {
+                const breweryInfoLi = document.createElement("li");
+                if (attributesList.includes(attr)) {
+                  breweryInfoLi.innerText = `${attr}: ${breweryDataObj[attr]}`;
+                  breweryInfoElement.appendChild(breweryInfoLi);
+                }
               }
-            }
+            });
+            // Add a mouseover and mouseout event listeners to each brewery to change color to blue
+            li.addEventListener("mouseover", (event) => {
+              event.target.style.color = "blue";
+            });
+            li.addEventListener("mouseout", (event) => {
+              event.target.style.color = "";
+            });
           });
-          li.addEventListener("mouseover", (event) => {
-            event.target.style.color = "blue";
-          });
-          li.addEventListener("mouseout", (event) => {
-            event.target.style.color = "";
-          });
-        });
+        }
       });
   };
 });
