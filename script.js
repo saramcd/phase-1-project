8237e94b68210f88f2538bebccb3dbe74ab9f041
Sara McDonnell <saramcdonnell32@gmail.com>
2023-03-22 12:23:32 -0400

    remove code comments

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   const breweryForm = document.getElementById("brewery-form");
   breweryForm.addEventListener("submit", (event) => {
-    // Prevents form from reloading when submitted
     event.preventDefault();
     const searchFormInput = document.querySelector("input#search").value;
     getBreweries(searchFormInput);
   });
 
   const getBreweries = (state) => {
-    // Clear results from the prior search
     const breweryElement = document.getElementById("breweries-list");
     breweryElement.innerHTML = "";
     const breweryInfoElement = document.getElementById("brewery-info");
     breweryInfoElement.innerHTML = "";
-    // Fetch brewery data by state
     fetch(
       `https://api.openbrewerydb.org/breweries?by_state=${state}&per_page=200`
     )
       .then((resp) => resp.json())
       .then((data) => {
         if (!data.length) {
-          // Alert the user if no breweries found for the state entered
           alert(
             `No breweries found for ${state}! Please enter one of the 50 states within the USA.`
           );
         } else {
-          // Populate HTML with requested state brewery data;
           const stateHeader = document.createElement("h3");
           stateHeader.innerText = `Breweries in ${state}:`;
 
           breweryElement.append(stateHeader);
-          // Create a List Item for each brewery name
           data.forEach((brewery) => {
             const li = document.createElement("li");
             li.innerText = brewery.name;
             li.setAttribute("id", brewery.id);
             breweryElement.appendChild(li);
-            // Add a click event listener to each brewery
             li.addEventListener("click", (event) => {
               const breweryClicked = event.target.innerText;
               const breweryData = data.filter(function (brewery) {
@@ -55,7 +48,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
               }
             });
-            // Add a mouseover and mouseout event listeners to each brewery to change color to blue
             li.addEventListener("mouseover", (event) => {
               event.target.style.color = "blue";
             });
